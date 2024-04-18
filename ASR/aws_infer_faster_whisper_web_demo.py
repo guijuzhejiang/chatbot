38ba50bc39e1473c712dfb3fbe8f21bc2e2e0c9e
ray <uubs@yeah.net>
2024-04-18 15:29:19 +0800

    update

--- ASR/aws_infer_faster_whisper_web_demo.py
+++ ASR/aws_infer_faster_whisper_web_demo.py
@@ -211,11 +211,11 @@ def audio_stream(*args, **kwargs):
             # buf_center[client_id]['data'] = bytearray()
             buf_center[client_id]['data'] = [audio_datas]
             buf_center[client_id]['data_len'] = get_wav_file_size(audio_datas)
-        print(buf_center[client_id])
+        # print(buf_center[client_id])
         chunk_length_in_bytes = chunk_length_seconds * sampling_rate * samples_width
 
-        print(f"buf_center[client_id]['data_len'] > chunk_length_in_bytes: {buf_center[client_id]['data_len'] > chunk_length_in_bytes}")
-        print(f"chunk_length_in_bytes : {chunk_length_in_bytes}")
+        # print(f"buf_center[client_id]['data_len'] > chunk_length_in_bytes: {buf_center[client_id]['data_len'] > chunk_length_in_bytes}")
+        # print(f"chunk_length_in_bytes : {chunk_length_in_bytes}")
 
         if buf_center[client_id]['data_len'] > chunk_length_in_bytes:
             # loop = asyncio.get_event_loop()
